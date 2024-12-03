ce0aad2e85782168afa8022b57866e0a3988ba8c
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2024-12-02 21:55:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mundo',
+    'O que é Lordran?',
+    'Lordran é o reino central de Dark Souls, conhecido como a terra dos antigos senhores e cheio de mistérios e perigos. É onde os jogadores exploram em busca de respostas e desafios.'
+);
+
+criaCartao(
+    'Maldição',
+    'O que é a maldição do Undead?',
+    'A maldição do Undead faz com que aqueles afetados não possam morrer permanentemente, mas percam sua humanidade e enlouqueçam se não recuperarem sua força de vontade.'
+);
+
+criaCartao(
+    'Personagens',
+    'Quem é Solaire de Astora?',
+    'Solaire de Astora é um cavaleiro carismático e otimista que busca seu "próprio sol". Ele é um dos personagens mais icônicos e queridos do jogo.'
+);
+
+criaCartao(
+    'Desafios',
+    'O que é o Bonfire?',
+    'As Bonfires são pontos de descanso e recuperação no mundo de Dark Souls. Elas permitem que os jogadores salvem seu progresso, recuperem energia, mas também revivem os inimigos.'
+);
